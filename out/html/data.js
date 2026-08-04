4c4786a2a1d4b94c6eb0d21934f0aab08ace3719
socialdemocracy <haeltor80@gmail.com>
2026-08-04 16:25:58 -0600

    Create data.js with colour and tooltip lists
    Added colourList and tooltipList for styling and explanations.

---
+++ out/html/data.js
@@ -0,0 +1,29 @@
+const colourList = [{
+        word: "PPS",
+        style: "color: #DD0000; font-weight: bold;"
+   },
+    {
+        word: "KPP",
+        style: "color: #890000; font-weight: bold;"
+   },
+    {
+        word: "ZLN",
+        style: "color: #312E99; font-weight: bold;"
+   },
+];
+
+
+
+const tooltipList = [{
+    searchString: "PPS",
+    explanationText: "<img src=img/pps.webp> Polska Partia Socjalistyczna </br> Polish Socialist Party"
+    },
+    {
+    searchString: "PSI",
+    explanationText: "<img src=img/kpp.jpg.png> Komunistyczna Partia Polski </br> Communist Party Of Poland"
+    },
+    {
+    searchString: "ZLN",
+    explanationText: "<img src=img/zln.webp> Związek Ludowo-Narodowy </br> Popular National Union"
+    },
+];
