8b7373f46dbfb658640eda7637837a47e99f3198
socialdemocracy <haeltor80@gmail.com>
2026-08-04 17:20:36 -0600

    Add new words and styles to data.js

--- out/html/data.js
+++ out/html/data.js
@@ -7,6 +7,46 @@ const colourList = [{
         style: "color: #890000; font-weight: bold;"
    },
     {
+        word: "NPR",
+        style: "color: #841839; font-weight: bold;"
+   },
+    {
+        word: "PSL-L",
+        style: "color: #80FF00; font-weight: bold;"
+   },
+    {
+        word: "PSL-W",
+        style: "color: #179900; font-weight: bold;"
+   },
+    {
+        word: "PSL-P",
+        style: "color: #3A6538; font-weight: bold;"
+   },
+    {
+        word: "BMN",
+        style: "color: #E3AF34; font-weight: bold;"
+   },
+    {
+        word: "ChZJN",
+        style: "color: #302060; font-weight: bold;"
+   },
+    {
+        word: "PSChD",
+        style: "color: #316EAA; font-weight: bold;"
+   },
+    {
+        word: "Other Parties",
+        style: "color: #A0A0E0; font-weight: bold;"
+   },
+    {
+        word: "Other",
+        style: "color: #909090; font-weight: bold;"
+   },
+    {
+        word: "Others",
+        style: "color: #909090; font-weight: bold;"
+   },
+    {
         word: "ZLN",
         style: "color: #312E99; font-weight: bold;"
    },
